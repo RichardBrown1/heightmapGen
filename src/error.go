f35929c95cc30f36675d31474278631012fc7aaa
richardbrown1 <richard987x@hotmail.com>
2020-12-27 10:31:49 +1100

    rearranged to only find ascii files

--- src/error.go
+++ src/error.go
@@ -2,6 +2,7 @@ package main
 
 import "log"
 
+//Check - panics if error is found
 func Check(err error) {
 	if err != nil {
 		log.Fatal(err)
--- src/files.go
+++ src/files.go
@@ -7,34 +7,20 @@ import (
 
 //GetAllSubDirs returns files, err
 func GetAllSubDirs(path string) ([]string, error) {
+	var files []string
 	fileInfo, err := ioutil.ReadDir(path)
 
-	var files []string
 	for _, file := range fileInfo {
 
-		files = append(files, path+"/"+file.Name())
+		if strings.HasSuffix(file.Name(), ".asc") {
+			files = append(files, path+"/"+file.Name())
+		}
 
-		if file.IsDir() || strings.HasSuffix(file.Name(), ".asc") {
+		if file.IsDir() {
 			var subdirFiles []string
-
-			if file.IsDir() {
-				subdirFiles, err = GetAllSubDirs(path + "/" + file.Name())
-			}
+			subdirFiles, err = GetAllSubDirs(path + "/" + file.Name())
 			files = append(files, subdirFiles...)
 		}
 	}
 	return files, err
 }
-
-// // ListSubDirs returns file, err
-// func ListSubDirs(path string) ([]string, error) {
-// 	fileInfo, err := ioutil.ReadDir(path)
-
-// 	var files []string
-// 	for _, file := range fileInfo {
-// 		if file.IsDir() {
-// 			files = append(files, file.Name())
-// 		}
-// 	}
-// 	return files, err
-// }
