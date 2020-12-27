8e713c9193aefe808c6a72c7ade3fe904cf84e4f
richardbrown1 <richard987x@hotmail.com>
2020-12-27 15:37:31 +1100

    added windows path seperator

--- src/files.go
+++ src/files.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"io/ioutil"
+	"runtime"
 	"strings"
 )
 
 //GetAllSubDirs returns files, err
 func GetAllSubDirs(path string) ([]string, error) {
 	var files []string
+	parentFolder := path
 	fileInfo, err := ioutil.ReadDir(path)
+
+	if runtime.GOOS == "windows" {
+		parentFolder += "\\"
+	} else {
+		parentFolder += "/"
+	}
 
 	for _, file := range fileInfo {
 
 		if strings.HasSuffix(file.Name(), ".asc") {
-			files = append(files, path+"/"+file.Name())
+			files = append(files, parentFolder+file.Name())
 		}
 
 		if file.IsDir() {
 			var subdirFiles []string
-			subdirFiles, err = GetAllSubDirs(path + "/" + file.Name())
+			subdirFiles, err = GetAllSubDirs(parentFolder + file.Name())
 			files = append(files, subdirFiles...)
 		}
 	}
