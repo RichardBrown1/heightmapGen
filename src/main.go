878f4917707e865b1dbd0583490c04a4033c7645
richardbrown1 <richard987x@hotmail.com>
2020-12-28 14:19:17 +1100

    grid now goes into memory

--- src/main.go
+++ src/main.go
@@ -45,13 +45,13 @@ func main() {
 		//Get ESRIInfo
 
 		//Getting right hand side of col - should validate but esrigrids are standardised somewhat
-		//scanner.Scan()
-		//scanner.Scan()
 		SkipAndScan(scanner, 1)
 		map1.ncols, err = strconv.Atoi(scanner.Text())
 
 		SkipAndScan(scanner, 1)
 		map1.nrows, err = strconv.Atoi(scanner.Text())
+
+		//map1.grid = map1.grid[:map1.nrows][:map1.ncols]
 
 		//TODO: There are xllcenter and yllcenter in some esri grids
 		SkipAndScan(scanner, 1)
@@ -64,14 +64,29 @@ func main() {
 		map1.cellsize, err = ParseFloat32(scanner.Text())
 
 		// //nodata_value can be missing depending on implementation
+		scanner.Scan()
 		if scanner.Text() == "nodata_value" {
 			scanner.Scan()
 			map1.noDataValue, err = ParseFloat32(scanner.Text())
 		} else {
+			fmt.Println("'", scanner.Text(), "'")
 			map1.noDataValue = noDataValueDefault
 		}
 
-		fmt.Println(map1.ncols, map1.nrows, map1.xllcorner, map1.noDataValue)
+		map1.grid = make([][]float32, map1.nrows)
 
+		for r := 1; r < map1.nrows; r++ {
+			map1.grid[r] = make([]float32, map1.ncols)
+			for c := 1; c < map1.ncols; c++ {
+				var v float32 //map1.grid[r][c]
+				v, err = ParseFloat32(scanner.Text())
+				map1.grid[r][c] = v
+				if !scanner.Scan() {
+					break
+				}
+			}
+		}
+		fmt.Println(map1.ncols, map1.nrows, map1.xllcorner, map1.noDataValue)
+		//fmt.Println(map1.grid)
 	}
 }
